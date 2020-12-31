f80cb0eb43ac7a2daf852ec26b6af7fc5f5d64be
Hans <fb_hans@outlook.com>
2020-12-31 21:00:34 +0800

    Update Models

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, blank=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, blank=True, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, blank=True, null=True)
@@ -23,21 +23,30 @@ class Tag(models.Model):
 
 
 class Product(models.Model):
+    CATEGORY = (
+        ('Outdoor', 'Outdoor'),
+        ('Indoor', 'Indoor'),
+    )
     name = models.CharField(max_length=200, null=True)
-    category = models.CharField(max_length=200, null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     price = models.FloatField(null=True, blank=True)
     description = models.CharField(max_length=200, blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
-    tag = models.ManyToManyField(Tag, null=True)
+    tag = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.name
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, blank=True, on_delete=models.SET_NULL)
-    product = models.ForeignKey(Customer, blank=True, on_delete=models.SET_NULL)
-    status = models.CharField(max_length=200, null=True)
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Delivered', 'Delivered'),
+        ('Out for Delivery', 'Out for Delivery'),
+    )
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
