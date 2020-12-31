49123eabfd79edcc30d3139d971fafafaa687309
Hans <fb_hans@outlook.com>
2020-12-31 22:36:51 +0800

    Update and Delete Order

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import *
+
 urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('customer/<str:pk>', customer, name='customer'),
     path('products/', products, name='products'),
-    path('create_orders/', create_orders, name='create_orders'),
+    path('create_orders/<str:pk>/', create_orders, name='create_orders'),
+    path('update_order/<str:pk>', update_order, name='update_order'),
+    path('delete_order/<str:pk>', delete_order, name='delete_order'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,32 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from django.forms import inlineformset_factory
+from .forms import *
 
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+    context = {
+
+    }
+    return render(request, 'accounts/login.html', context)
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+
 def dashboard(request):
     customers = Customer.objects.all()
     orders = Order.objects.all().order_by('-date_created')[:5]
@@ -25,7 +48,7 @@ def dashboard(request):
 
 def customer(request, pk):
     customer = Customer.objects.get(pk=pk)
-    orders = customer.order_set.all()
+    orders = customer.order_set.all().order_by('-date_created')
     total_orders = orders.count()
 
     context = {
@@ -44,10 +67,46 @@ def products(request):
     return render(request, 'accounts/products.html', context)
 
 
-def create_orders(request):
+def create_orders(request, pk):
+    customer = Customer.objects.get(pk=pk)
     OrderForm = inlineformset_factory(Customer, Order, fields='__all__', exclude=['customer'], extra=5)
+    formset = OrderForm(instance=customer, queryset=customer.order_set.none())
+
+    if request.method == 'POST':
+        formset = OrderForm(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
+            return redirect('dashboard')
 
     context = {
-
+        'formset': formset,
     }
     return render(request, 'accounts/create_orders.html', context)
+
+
+def update_order(request, pk):
+    order = Order.objects.get(pk=pk
+                              )
+    form = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update_order.html', context)
+
+
+def delete_order(request, pk):
+    order = Order.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        order.delete()
+        return redirect('dashboard')
+    context = {
+        'item': order,
+    }
+    return render(request, 'accounts/delete_order.html', context)
